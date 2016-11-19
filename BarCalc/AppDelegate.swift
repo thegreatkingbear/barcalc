764f26708dc6b339bac5d6c9363d4cbef8b61dfd
Mookyung Kwak <mookyungkwak@node-1w7jr9qoz93oj7iepz3jfwfrl.ipv6.telus.net>
2016-11-19 10:20:44 -0800

    Initial Commit

---
+++ BarCalc/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BarCalc
+//
+//  Created by Mookyung Kwak on 19/11/2016.
+//  Copyright © 2016 Mookyung Kwak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
