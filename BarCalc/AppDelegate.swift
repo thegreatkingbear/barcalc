7ed9a43b7dbfe16f83e2525d7fd107972cf71b0a
Mookyung Kwak <mookyungkwak@node-1w7jr9qoz93oj7iepz3jfwfrl.ipv6.telus.net>
2016-11-19 22:45:38 -0800

    메뉴바에서 계산을 할수있는 심플한 기능을 넣어서 만들었음 다음에는 reminder나 note, calendar에 넣을 수 있게끔 만들어보자

--- BarCalc/AppDelegate.swift
+++ BarCalc/AppDelegate.swift
@@ -11,17 +11,43 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
+    
     @IBOutlet weak var window: NSWindow!
 
-
+    let popover = NSPopover()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = #selector(AppDelegate.toggleTextInput(_:))
+        }
+        
+        popover.contentViewController = TextInputViewController(nibName: "TextInputViewController", bundle: nil)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
-
+    func toggleTextInput(sender: AnyObject?) {
+        print("start calculating")
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
+    
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
 }
 
---
+++ BarCalc/TextInputViewController.swift
@@ -0,0 +1,64 @@
+//
+//  TextInputViewController.swift
+//  BarCalc
+//
+//  Created by Mookyung Kwak on 19/11/2016.
+//  Copyright © 2016 Mookyung Kwak. All rights reserved.
+//
+
+import Cocoa
+
+class TextInputViewController: NSViewController, NSTextFieldDelegate {
+
+    @IBOutlet weak var textField: NSTextField?
+    
+    var existingExpression: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        textField?.delegate = self
+    }
+    
+    override func controlTextDidBeginEditing(obj: NSNotification) {
+        //print("control text did begin editing")
+    }
+    
+    override func controlTextDidEndEditing(obj: NSNotification) {
+        //print("control text did end editing")
+        if let txtFld = obj.object as? NSTextField {
+            let text = txtFld.stringValue
+            self.calculateExpression(text)
+        }
+    }
+    
+    override func controlTextDidChange(obj: NSNotification) {
+    }
+    
+    func control(control: NSControl, textView: NSTextView, doCommandBySelector commandSelector: Selector) -> Bool {
+        print("do command by selector : \(control)")
+        if let existing = existingExpression {
+            self.textField?.stringValue = existing
+            self.existingExpression = nil
+        }
+        return false
+    }
+    
+    func calculateExpression(values: String) {
+        SwiftTryCatch.tryBlock({ 
+            let expression = NSExpression(format: values)
+            if let result = expression.expressionValueWithObject(nil, context: nil) as? NSNumber {
+                self.textField?.stringValue = "\(expression) = \(result.stringValue)"
+                self.existingExpression = "\(expression)"
+            } else {
+                self.textField?.stringValue = "Invalid expression"
+                self.existingExpression = nil
+            }
+        }, catchBlock: { (error) in
+            self.textField?.stringValue = "Invalid expression"
+            self.existingExpression = nil
+        }) {
+            
+        }
+    }
+}
